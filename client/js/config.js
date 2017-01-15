81dc8906a332297efe4f66e4f7a5d56e42993dd6
marosan <marcelo.oliveira@salesforce.com>
2017-01-15 09:46:03 -0200

    Update config.js

--- client/js/config.js
+++ client/js/config.js
@@ -2,16 +2,16 @@ angular.module('nibs.config', [])
 
     .constant('SERVER_URL', null)
 
-    .constant('FB_APP_ID','1618878675020266')
+    .constant('FB_APP_ID','1886532414898182')
 
     .constant('STATUS_LABELS', [
-        'Forastero',
-        'Trinitario',
-        'Criollo'
+        'Super',
+        'Ultra',
+        'VIP'
     ])
 
     .constant('STATUS_DESCRIPTIONS', [
-        'Forastero (For-ah-stare-oh)  is a common base to all but the finest of chocolates.  Our newest Nibs members move as fast as Forastero pods grow.',
-        'Trinitario (Trin-it-air-ee-yo)  is a hybrid combining the superior taste of the criollo bean with the resilience of the forastero bean. It’s not quite as rare as Criollo, but it’s close!',
-        'Criollo (Kree-oh-yo) is the most valued and rare type of cacao. Our highest level of Nibs members share the taste and sophistication of  select Crillolo pods.'
+        'Super. Clientes Super iniciaram a jornada com a Ultragaz, mas rapidamente se tornam ultra !',
+        'Ultra. Compartilham suas experiência com as receitas e já realizam alguns pedidos. ,
+        'VIP. Estão em constante contato com a Ultra e recebem grandes vantagens.'
     ]);
